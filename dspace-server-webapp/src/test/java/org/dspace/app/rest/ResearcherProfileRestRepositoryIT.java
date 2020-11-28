b0568b41fc9b97d819f2a3ffca82c864e4e1b6fe
Corrado Lombardi <corrado.lombardi@4science.it>
2020-11-28 19:45:58 +0100

    [CSTPER-283] esternal-import: added cases of wrong and multiple URIs

--- dspace-server-webapp/src/test/java/org/dspace/app/rest/ResearcherProfileRestRepositoryIT.java
+++ dspace-server-webapp/src/test/java/org/dspace/app/rest/ResearcherProfileRestRepositoryIT.java
@@ -23,6 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.io.UnsupportedEncodingException;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import com.jayway.jsonpath.JsonPath;
@@ -33,11 +34,21 @@ import org.dspace.app.rest.model.patch.ReplaceOperation;
 import org.dspace.app.rest.test.AbstractControllerIntegrationTest;
 import org.dspace.builder.CollectionBuilder;
 import org.dspace.builder.CommunityBuilder;
+import org.dspace.builder.CrisLayoutBoxBuilder;
+import org.dspace.builder.CrisLayoutFieldBuilder;
 import org.dspace.builder.EPersonBuilder;
+import org.dspace.builder.EntityTypeBuilder;
 import org.dspace.builder.ItemBuilder;
 import org.dspace.content.Collection;
+import org.dspace.content.EntityType;
 import org.dspace.content.Item;
+import org.dspace.content.MetadataField;
+import org.dspace.content.MetadataSchema;
+import org.dspace.content.service.MetadataFieldService;
+import org.dspace.content.service.MetadataSchemaService;
 import org.dspace.eperson.EPerson;
+import org.dspace.layout.CrisLayoutBox;
+import org.dspace.layout.LayoutSecurity;
 import org.dspace.services.ConfigurationService;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -56,6 +67,12 @@ public class ResearcherProfileRestRepositoryIT extends AbstractControllerIntegra
 
     @Autowired
     private ConfigurationService configurationService;
+
+    @Autowired
+    private MetadataSchemaService metadataSchemaService;
+
+    @Autowired
+    private MetadataFieldService metadataFieldService;
 
     private EPerson user;
 
@@ -701,43 +718,88 @@ public class ResearcherProfileRestRepositoryIT extends AbstractControllerIntegra
 
         context.turnOffAuthorisationSystem();
         Item person = ItemBuilder.createItem(context, personCollection)
-                .withFullName("Mario Rossi")
-                .withRelationshipType("Person")
-                .withOrcidIdentifier("0000-1234-1234-1111")
-                .withBirthDate("1982-12-17").build();
+            .withFullName("Giuseppe Garibaldi")
+            .withRelationshipType("Person")
+            .withBirthDate("1807-07-04")
+            .withOrcidIdentifier("0000-1111-2222-3333")
+            .build();
+
+        EntityType entityType = EntityTypeBuilder.createEntityTypeBuilder(context, "Person").build();
+
+        CrisLayoutBox publicBox = CrisLayoutBoxBuilder.createBuilder(context, entityType, false, false)
+            .withSecurity(LayoutSecurity.PUBLIC).build();
+
+        CrisLayoutBox ownerAndAdministratorBox = CrisLayoutBoxBuilder.createBuilder(context, entityType, false, false)
+            .withSecurity(LayoutSecurity.OWNER_AND_ADMINISTRATOR).build();
+
+
+        CrisLayoutFieldBuilder.createMetadataField(context,
+            metadataField("crisrp", "name", Optional.empty()),
+            1, 1)
+            .withBox(publicBox)
+            .build();
+
+        CrisLayoutFieldBuilder.createMetadataField(context,
+            metadataField("person", "birthDate", Optional.empty()),
+            2, 1)
+            .withBox(publicBox).build();
+
+        CrisLayoutFieldBuilder.createMetadataField(context,
+            metadataField("perucris", "identifier", Optional.of("dni")),
+            1, 1)
+            .withBox(ownerAndAdministratorBox).build();
+
         context.restoreAuthSystemState();
 
         String authToken = getAuthToken(user.getEmail(), password);
 
         getClient(authToken).perform(post("/api/cris/profiles/")
-                .contentType(TEXT_URI_LIST).content("http://localhost:8080/server/api/integration/externalsources/orcid/entryValues/0000-1234-1234-1111"))
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.id", is(user.getID())))
-                .andExpect(jsonPath("$.visible", is(false)))
-                .andExpect(jsonPath("$.type", is("item")))
-                .andExpect(jsonPath("$.metadata['person.birthDate']", is("1982-12-17")))
-                .andExpect(jsonPath("$.metadata['crisrp.name']", is("Mario Rossi")))
-                .andExpect(jsonPath("$", matchLinks("http://localhost/api/cris/profiles/" + user.getID(), "item", "eperson")));
+            .contentType(TEXT_URI_LIST).content("http://localhost:8080/server/api/integration/externalsources/orcid/entryValues/0000-1111-2222-3333"))
+            .andExpect(status().isCreated())
+            .andExpect(jsonPath("$.id", is(user.getID().toString())))
+            .andExpect(jsonPath("$.visible", is(false)))
+            .andExpect(jsonPath("$.type", is("profile")))
+            .andExpect(jsonPath("$", matchLinks("http://localhost/api/cris/profiles/" + user.getID(), "item", "eperson")));
 
-//        getClient(authToken).perform(get("/api/cris/profiles/{id}", id))
-//                .andExpect(status().isOk());
-//
-//        getClient(authToken).perform(get("/api/cris/profiles/{id}/item", id))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.type", is("item")))
-//                .andExpect(jsonPath("$.metadata", matchMetadata("cris.owner", name, id.toString(), 0)))
-//                .andExpect(jsonPath("$.metadata", matchMetadata("cris.sourceId", id, 0)))
-//                .andExpect(jsonPath("$.metadata", matchMetadata("relationship.type", "Person", 0)));
-//
-//        getClient(authToken).perform(get("/api/cris/profiles/{id}/eperson", id))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.type", is("eperson")))
-//                .andExpect(jsonPath("$.name", is(name)));
+        getClient(authToken).perform(get("/api/cris/profiles/{id}", user.getID()))
+            .andExpect(status().isOk());
+
+        getClient(authToken).perform(get("/api/cris/profiles/{id}/item", user.getID()))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.type", is("item")))
+            .andExpect(jsonPath("$.metadata", matchMetadata("cris.owner", user.getName(), user.getID().toString(), 0)))
+            .andExpect(jsonPath("$.metadata", matchMetadata("crisrp.name", "Giuseppe Garibaldi", 0)))
+            .andExpect(jsonPath("$.metadata", matchMetadata("relationship.type", "Person", 0)))
+            .andExpect(jsonPath("$.metadata", matchMetadata("person.birthDate", "1807-07-04", 0)));
+
+        getClient(authToken).perform(get("/api/cris/profiles/{id}/eperson", user.getID()))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.type", is("eperson")))
+            .andExpect(jsonPath("$.name", is(user.getName())));
     }
 
-    // test with empty source list
+    @Test
+    public void testCloneFromExternalSourceRecordNotFound() throws Exception {
 
-    // test with multiple sources
+        String authToken = getAuthToken(user.getEmail(), password);
+
+        getClient(authToken).perform(post("/api/cris/profiles/")
+            .contentType(TEXT_URI_LIST).content("http://localhost:8080/server/api/integration/externalsources/orcid/entryValues/FAKE"))
+            .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testCloneFromExternalSourceMultipleUri() throws Exception {
+
+        String authToken = getAuthToken(user.getEmail(), password);
+
+        getClient(authToken).perform(post("/api/cris/profiles/")
+            .contentType(TEXT_URI_LIST)
+            .content("http://localhost:8080/server/api/integration/externalsources/orcid/entryValues/id \n " +
+                "http://localhost:8080/server/api/integration/externalsources/dspace/entryValues/id"))
+            .andExpect(status().isBadRequest());
+
+    }
 
     private String getItemIdByProfileId(String token, String id) throws SQLException, Exception {
         MvcResult result = getClient(token).perform(get("/api/cris/profiles/{id}/item", id))
@@ -749,5 +811,16 @@ public class ResearcherProfileRestRepositoryIT extends AbstractControllerIntegra
 
     private <T> T readAttributeFromResponse(MvcResult result, String attribute) throws UnsupportedEncodingException {
         return JsonPath.read(result.getResponse().getContentAsString(), attribute);
+    }
+
+    private MetadataField metadataField(String schema, String element, Optional<String> qualifier)
+        throws SQLException {
+
+        MetadataSchema metadataSchema = metadataSchemaService.find(context, schema);
+
+        return metadataFieldService.findByElement(context,
+            metadataSchema,
+            element,
+            qualifier.orElse(null));
     }
 }

1410e6dc58bf0187d01a850fa0ee50d5bd72360d
Corrado Lombardi <corrado.lombardi@4science.it>
2020-11-29 18:18:28 +0100

    [CSTPER-283] added ExternalDataSource to AfterImportAction contract

--- dspace-api/src/test/java/org/dspace/app/profile/service/ImportResearcherProfileServiceImplTest.java
+++ dspace-api/src/test/java/org/dspace/app/profile/service/ImportResearcherProfileServiceImplTest.java
@@ -59,7 +59,7 @@ public class ImportResearcherProfileServiceImplTest {
         URI source = URI.create("http://localhost:8080/path_to_external/serviceId/entry/1234");
         Collection collection = mock(Collection.class);
 
-        ExternalDataObject externalDataObject = externalDataObject("1234");
+        ExternalDataObject externalDataObject = createExternalDataObject("1234");
         when(externalDataService.getExternalDataObject("serviceId", "1234"))
                 .thenReturn(Optional.of(externalDataObject));
 
@@ -98,7 +98,7 @@ public class ImportResearcherProfileServiceImplTest {
         URI source = URI.create("http://localhost:8080/path_to_external/serviceId/entry/9999");
         Collection collection = mock(Collection.class);
 
-        ExternalDataObject externalDataObject = externalDataObject("9999");
+        ExternalDataObject externalDataObject = createExternalDataObject("9999");
 
         when(externalDataService.getExternalDataObject("serviceId", "9999"))
                 .thenReturn(Optional.of(externalDataObject));
@@ -122,7 +122,7 @@ public class ImportResearcherProfileServiceImplTest {
         URI source = URI.create("http://localhost:8080/path_to_external/serviceId/entry/1234");
         Collection collection = mock(Collection.class);
 
-        ExternalDataObject externalDataObject = externalDataObject("1234");
+        ExternalDataObject externalDataObject = createExternalDataObject("1234");
         when(externalDataService.getExternalDataObject("serviceId", "1234"))
                 .thenReturn(Optional.of(externalDataObject));
 
@@ -138,10 +138,10 @@ public class ImportResearcherProfileServiceImplTest {
         importResearcherProfileService.importProfile(context, source,
                 collection);
 
-        verify(afterImportAction).applyTo(item);
+        verify(afterImportAction).applyTo(item, externalDataObject);
     }
 
-    private ExternalDataObject externalDataObject(String s) {
+    private ExternalDataObject createExternalDataObject(String s) {
         ExternalDataObject externalDataObject = new ExternalDataObject();
         externalDataObject.setId(s);
         return externalDataObject;

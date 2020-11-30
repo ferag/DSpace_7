272dc1ca1ecda56651645bdf515eb66be3759eca
Corrado Lombardi <corrado.lombardi@4science.it>
2020-11-30 15:52:12 +0100

    [CSTPER-283] fix on StringUtils import

--- dspace-api/src/main/java/org/dspace/importer/external/dspace/DspaceAfterImportAction.java
+++ dspace-api/src/main/java/org/dspace/importer/external/dspace/DspaceAfterImportAction.java
@@ -15,7 +15,7 @@ import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.dspace.app.profile.service.AfterImportAction;
 import org.dspace.authorize.AuthorizeException;
 import org.dspace.content.EntityType;

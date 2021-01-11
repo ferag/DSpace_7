6ad4e1a8bed2fb8545469813d1238699e18328fd
Corrado Lombardi <corrado.lombardi@4science.it>
2021-01-11 17:37:35 +0100

    added full projection in create empty workspace item request

--- dspace-server-webapp/src/test/java/org/dspace/app/rest/WorkspaceItemRestRepositoryIT.java
+++ dspace-server-webapp/src/test/java/org/dspace/app/rest/WorkspaceItemRestRepositoryIT.java
@@ -5333,7 +5333,7 @@ public class WorkspaceItemRestRepositoryIT extends AbstractControllerIntegration
     }
 
     @Test
-    public void createEmptyWorkspateItemWithEntityTypeTest() throws Exception {
+    public void createEmptyWorkspaceItemWithEntityTypeTest() throws Exception {
         context.turnOffAuthorisationSystem();
 
         //** GIVEN **
@@ -5366,6 +5366,7 @@ public class WorkspaceItemRestRepositoryIT extends AbstractControllerIntegration
             // create a workspaceitem explicitly with entityType Publication
             getClient(authToken).perform(post("/api/submission/workspaceitems")
                     .param("entityType", "Publication")
+                    .param("projection", "full")
                     .contentType(org.springframework.http.MediaType.APPLICATION_JSON))
                     .andExpect(status().isCreated())
                     .andExpect(jsonPath("$._embedded.collection.metadata.['relationship.type'][0].value",

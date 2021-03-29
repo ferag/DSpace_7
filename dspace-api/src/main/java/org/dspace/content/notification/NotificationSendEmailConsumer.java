121cb7b46208d87b6da46c930795980760c001e8
Corrado Lombardi <corrado.lombardi@4science.it>
2021-03-29 15:04:33 +0200

    [CSTPER-527] added message in case of error while sending notification

--- dspace-api/src/main/java/org/dspace/content/notification/NotificationSendEmailConsumer.java
+++ dspace-api/src/main/java/org/dspace/content/notification/NotificationSendEmailConsumer.java
@@ -6,11 +6,15 @@
  * http://www.dspace.org/license/
  */
 package org.dspace.content.notification;
+
+import java.io.IOException;
+import java.sql.SQLException;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Set;
 import java.util.UUID;
+import javax.mail.MessagingException;
 
 import org.apache.commons.lang3.StringUtils;
 import org.dspace.content.DSpaceObject;
@@ -26,6 +30,10 @@ import org.dspace.eperson.factory.EPersonServiceFactory;
 import org.dspace.eperson.service.EPersonService;
 import org.dspace.event.Consumer;
 import org.dspace.event.Event;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 
 /**
  * Consumer that takes care of send Notification via email.
@@ -33,6 +41,8 @@ import org.dspace.event.Event;
  * @author Mykhaylo Boychuk (mykhaylo.boychuk at 4science.it)
  */
 public class NotificationSendEmailConsumer implements Consumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(NotificationSendEmailConsumer.class);
 
     private Set<UUID> itemsAlreadyProcessed;
 
@@ -60,27 +70,36 @@ public class NotificationSendEmailConsumer implements Consumer {
             if (item.isArchived() && isNotificationEntityItem(item) && !itemsAlreadyProcessed.contains(item.getID())) {
                 context.turnOffAuthorisationSystem();
                 List<MetadataValue> list = itemService.getMetadataByMetadataString(item, "perucris.notification.to");
-                if (!list.isEmpty()) {
-                    UUID cvPersonitemUuid = UUID.fromString(list.get(0).getAuthority());
-                    Item cvPersonItem = itemService.find(context, cvPersonitemUuid);
-                    List<MetadataValue> crisOwner = itemService.getMetadata(cvPersonItem, "cris", "owner", null, null);
-                    if (!crisOwner.isEmpty()) {
-                        UUID ePersonUuid = UUID.fromString(crisOwner.get(0).getAuthority());
-                        EPerson cvOwner = ePersonService.find(context, ePersonUuid);
-                        Locale supportedLocale = I18nUtil.getEPersonLocale(cvOwner);
-                        Email email = Email.getEmail(I18nUtil.getEmailFilename(supportedLocale, "notification_email"));
-                        email.addArgument(itemService.getMetadataFirstValue(item, "perucris", "notification", "message",
-                                Item.ANY));
-                        email.addRecipient(cvOwner.getEmail());
-                        email.addArgument(cvOwner);
-                        email.send();
+                try {
+                    if (!list.isEmpty()) {
+                        sendEmail(context, item, list);
                     }
-
+                } catch (Exception e) {
+                    LOGGER.error("Error while sending notification : {}", e.getMessage(), e);
+                }  finally {
+                    itemsAlreadyProcessed.add(item.getID());
+                    context.restoreAuthSystemState();
                 }
-                itemsAlreadyProcessed.add(item.getID());
-                context.restoreAuthSystemState();
             }
 
+        }
+    }
+
+    private void sendEmail(Context context, Item item, List<MetadataValue> list)
+        throws SQLException, IOException, MessagingException {
+        UUID cvPersonitemUuid = UUID.fromString(list.get(0).getAuthority());
+        Item cvPersonItem = itemService.find(context, cvPersonitemUuid);
+        List<MetadataValue> crisOwner = itemService.getMetadata(cvPersonItem, "cris", "owner", null, null);
+        if (!crisOwner.isEmpty()) {
+            UUID ePersonUuid = UUID.fromString(crisOwner.get(0).getAuthority());
+            EPerson cvOwner = ePersonService.find(context, ePersonUuid);
+            Locale supportedLocale = I18nUtil.getEPersonLocale(cvOwner);
+            Email email = Email.getEmail(I18nUtil.getEmailFilename(supportedLocale, "notification_email"));
+            email.addArgument(itemService.getMetadataFirstValue(item, "perucris", "notification", "message",
+                    Item.ANY));
+            email.addRecipient(cvOwner.getEmail());
+            email.addArgument(cvOwner);
+            email.send();
         }
     }
 

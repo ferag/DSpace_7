37afeddcecf6cee9f511f0d83e405fef32304d20
Mykhaylo <mykhaylo.boychuk@4science.it>
2021-05-27 12:41:04 +0200

    [CSTPER-667] update flyway scripts

--- dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/oracle/V7.0_2021.05.17__elasticsearch_index_queue.sql
+++ dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/oracle/V7.0_2021.05.17__elasticsearch_index_queue.sql
@@ -10,18 +10,9 @@
 -- Create tables for ElasticsearchIndex Queue
 -----------------------------------------------------------------------------------
 
-CREATE SEQUENCE elasticsearch_queue_id_seq;
-
 CREATE TABLE elasticsearch_index_queue
 (
-    id INTEGER NOT NULL,
-    item_id uuid NOT NULL,
+    item_uuid UUID NOT NULL primary key,
     operation_type INTEGER,
-    timestamp_insertion_date TIMESTAMP,
-    CONSTRAINT elasticsearch_index_queue_pkey PRIMARY KEY (id),
-    CONSTRAINT elasticsearch_queue_item_id_fkey FOREIGN KEY (item_id) REFERENCES item (uuid)
+    timestamp_insertion_date TIMESTAMP
 );
-
-CREATE INDEX elasticsearch_queue_item_id_index on elasticsearch_index_queue(item_id);
-
-CREATE SEQUENCE elasticsearch_index_queue_id_seq;

fcfb2476f918d8c94376a8ba36d47cbb2fac1a81
Mykhaylo <mykhaylo.boychuk@4science.it>
2021-08-24 18:33:44 +0200

    [CSTPER-759] added tests

--- dspace-server-webapp/src/test/java/org/dspace/app/rest/EntityTypeRestRepositoryIT.java
+++ dspace-server-webapp/src/test/java/org/dspace/app/rest/EntityTypeRestRepositoryIT.java
@@ -14,6 +14,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.Objects;
+
 import org.dspace.app.rest.matcher.EntityTypeMatcher;
 import org.dspace.app.rest.test.AbstractEntityIntegrationTest;
 import org.dspace.builder.CollectionBuilder;
@@ -40,6 +42,14 @@ public class EntityTypeRestRepositoryIT extends AbstractEntityIntegrationTest {
     private EntityType personType;
     private EntityType journalVolumeType;
     private EntityType projectType;
+    private EntityType CvPersonType;
+    private EntityType CvPersonCloneType;
+    private EntityType CvPublicationType;
+    private EntityType CvPublicationCloneType;
+    private EntityType CvProjectType;
+    private EntityType CvProjectCloneType;
+    private EntityType CvPatentType;
+    private EntityType CvPatentCloneType;
 
     @Before
     @Override
@@ -75,6 +85,38 @@ public class EntityTypeRestRepositoryIT extends AbstractEntityIntegrationTest {
         if (orgUnitType == null) {
             orgUnitType = EntityTypeBuilder.createEntityTypeBuilder(context, "OrgUnit").build();
         }
+        CvPersonType = entityTypeService.findByEntityType(context, "CvPerson");
+        if (Objects.isNull(CvPersonType)) {
+            CvPersonType = EntityTypeBuilder.createEntityTypeBuilder(context, "CvPerson").build();
+        }
+        CvPersonCloneType = entityTypeService.findByEntityType(context, "CvPersonClone");
+        if (Objects.isNull(CvPersonCloneType)) {
+            CvPersonCloneType = EntityTypeBuilder.createEntityTypeBuilder(context, "CvPersonClone").build();
+        }
+        CvPublicationType = entityTypeService.findByEntityType(context, "CvPublication");
+        if (Objects.isNull(CvPublicationType)) {
+            CvPublicationType = EntityTypeBuilder.createEntityTypeBuilder(context, "CvPublication").build();
+        }
+        CvPublicationCloneType = entityTypeService.findByEntityType(context, "CvPublicationClone");
+        if (Objects.isNull(CvPublicationCloneType)) {
+            CvPublicationCloneType = EntityTypeBuilder.createEntityTypeBuilder(context, "CvPublicationClone").build();
+        }
+        CvProjectType = entityTypeService.findByEntityType(context, "CvProject");
+        if (Objects.isNull(CvProjectType)) {
+            CvProjectType = EntityTypeBuilder.createEntityTypeBuilder(context, "CvProject").build();
+        }
+        CvProjectCloneType = entityTypeService.findByEntityType(context, "CvProjectClone");
+        if (Objects.isNull(CvProjectCloneType)) {
+            CvProjectCloneType = EntityTypeBuilder.createEntityTypeBuilder(context, "CvProjectClone").build();
+        }
+        CvPatentType = entityTypeService.findByEntityType(context, "CvPatent");
+        if (Objects.isNull(CvPatentType)) {
+            CvPatentType = EntityTypeBuilder.createEntityTypeBuilder(context, "CvPatent").build();
+        }
+        CvPatentCloneType = entityTypeService.findByEntityType(context, "CvPatentClone");
+        if (Objects.isNull(CvPatentCloneType)) {
+            CvPatentCloneType = EntityTypeBuilder.createEntityTypeBuilder(context, "CvPatentClone").build();
+        }
 
         context.restoreAuthSystemState();
     }
@@ -87,7 +129,7 @@ public class EntityTypeRestRepositoryIT extends AbstractEntityIntegrationTest {
                    //We expect a 200 OK status
                    .andExpect(status().isOk())
                    //The type has to be 'discover'
-                   .andExpect(jsonPath("$.page.totalElements", is(7)))
+                   .andExpect(jsonPath("$.page.totalElements", is(15)))
                    //There needs to be a self link to this endpoint
                    .andExpect(jsonPath("$._links.self.href", containsString("api/core/entitytypes")))
                    //We have 4 facets in the default configuration, they need to all be present in the embedded section
@@ -101,7 +143,20 @@ public class EntityTypeRestRepositoryIT extends AbstractEntityIntegrationTest {
                        EntityTypeMatcher
                            .matchEntityTypeEntry(entityTypeService.findByEntityType(context, "JournalVolume")),
                        EntityTypeMatcher
-                           .matchEntityTypeEntry(entityTypeService.findByEntityType(context, "JournalIssue"))
+                           .matchEntityTypeEntry(entityTypeService.findByEntityType(context, "JournalIssue")),
+                       EntityTypeMatcher.matchEntityTypeEntry(entityTypeService.findByEntityType(context, "CvPerson")),
+                       EntityTypeMatcher
+                           .matchEntityTypeEntry(entityTypeService.findByEntityType(context, "CvPersonClone")),
+                       EntityTypeMatcher
+                           .matchEntityTypeEntry(entityTypeService.findByEntityType(context, "CvPublication")),
+                       EntityTypeMatcher
+                           .matchEntityTypeEntry(entityTypeService.findByEntityType(context, "CvPublicationClone")),
+                       EntityTypeMatcher.matchEntityTypeEntry(entityTypeService.findByEntityType(context, "CvProject")),
+                       EntityTypeMatcher
+                           .matchEntityTypeEntry(entityTypeService.findByEntityType(context, "CvProjectClone")),
+                       EntityTypeMatcher.matchEntityTypeEntry(entityTypeService.findByEntityType(context, "CvPatent")),
+                       EntityTypeMatcher
+                           .matchEntityTypeEntry(entityTypeService.findByEntityType(context, "CvPatentClone"))
                    )));
     }
 
@@ -113,8 +168,8 @@ public class EntityTypeRestRepositoryIT extends AbstractEntityIntegrationTest {
                    .andExpect(status().isOk())
                    //The type has to be 'discover'
                    .andExpect(jsonPath("$.page.size", is(5)))
-                   .andExpect(jsonPath("$.page.totalElements", is(7)))
-                   .andExpect(jsonPath("$.page.totalPages", is(2)))
+                   .andExpect(jsonPath("$.page.totalElements", is(15)))
+                   .andExpect(jsonPath("$.page.totalPages", is(3)))
                    //There needs to be a self link to this endpoint
                    .andExpect(jsonPath("$._links.self.href", containsString("api/core/entitytypes")))
                    //We have 4 facets in the default configuration, they need to all be present in the embedded section
@@ -133,8 +188,8 @@ public class EntityTypeRestRepositoryIT extends AbstractEntityIntegrationTest {
                    .andExpect(status().isOk())
                    //The type has to be 'discover'
                    .andExpect(jsonPath("$.page.size", is(5)))
-                   .andExpect(jsonPath("$.page.totalElements", is(7)))
-                   .andExpect(jsonPath("$.page.totalPages", is(2)))
+                   .andExpect(jsonPath("$.page.totalElements", is(15)))
+                   .andExpect(jsonPath("$.page.totalPages", is(3)))
                    .andExpect(jsonPath("$.page.number", is(1)))
                    //There needs to be a self link to this endpoint
                    .andExpect(jsonPath("$._links.self.href", containsString("api/core/entitytypes")))
@@ -143,7 +198,13 @@ public class EntityTypeRestRepositoryIT extends AbstractEntityIntegrationTest {
                        EntityTypeMatcher
                            .matchEntityTypeEntry(entityTypeService.findByEntityType(context, "JournalVolume")),
                        EntityTypeMatcher
-                           .matchEntityTypeEntry(entityTypeService.findByEntityType(context, "JournalIssue"))
+                            .matchEntityTypeEntry(entityTypeService.findByEntityType(context, "JournalIssue")),
+                       EntityTypeMatcher
+                                .matchEntityTypeEntry(entityTypeService.findByEntityType(context, "CvPerson")),
+                       EntityTypeMatcher
+                           .matchEntityTypeEntry(entityTypeService.findByEntityType(context, "CvPersonClone")),
+                       EntityTypeMatcher
+                           .matchEntityTypeEntry(entityTypeService.findByEntityType(context, "CvPublication"))
                    )));
     }
 
@@ -234,10 +295,10 @@ public class EntityTypeRestRepositoryIT extends AbstractEntityIntegrationTest {
                            Matchers.containsString("page=1"), Matchers.containsString("size=3"))))
                    .andExpect(jsonPath("$._links.last.href", Matchers.allOf(
                            Matchers.containsString("/api/core/entitytypes?"),
-                           Matchers.containsString("page=2"), Matchers.containsString("size=3"))))
+                           Matchers.containsString("page=4"), Matchers.containsString("size=3"))))
                    .andExpect(jsonPath("$.page.size", is(3)))
-                   .andExpect(jsonPath("$.page.totalElements", is(7)))
-                   .andExpect(jsonPath("$.page.totalPages", is(3)))
+                   .andExpect(jsonPath("$.page.totalElements", is(15)))
+                   .andExpect(jsonPath("$.page.totalPages", is(5)))
                    .andExpect(jsonPath("$.page.number", is(0)));
 
         getClient().perform(get("/api/core/entitytypes")
@@ -263,11 +324,143 @@ public class EntityTypeRestRepositoryIT extends AbstractEntityIntegrationTest {
                            Matchers.containsString("page=2"), Matchers.containsString("size=3"))))
                    .andExpect(jsonPath("$._links.last.href", Matchers.allOf(
                            Matchers.containsString("/api/core/entitytypes?"),
-                           Matchers.containsString("page=2"), Matchers.containsString("size=3"))))
+                           Matchers.containsString("page=4"), Matchers.containsString("size=3"))))
                    .andExpect(jsonPath("$.page.size", is(3)))
-                   .andExpect(jsonPath("$.page.totalElements", is(7)))
-                   .andExpect(jsonPath("$.page.totalPages", is(3)))
+                   .andExpect(jsonPath("$.page.totalElements", is(15)))
+                   .andExpect(jsonPath("$.page.totalPages", is(5)))
                    .andExpect(jsonPath("$.page.number", is(1)));
+
+        getClient().perform(get("/api/core/entitytypes")
+                .param("page", "2")
+                .param("size", "3"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$._embedded.entitytypes", containsInAnyOrder(
+                 EntityTypeMatcher.matchEntityTypeEntry(entityTypeService.findByEntityType(context, "JournalIssue")),
+                 EntityTypeMatcher.matchEntityTypeEntry(entityTypeService.findByEntityType(context, "CvPerson")),
+                 EntityTypeMatcher.matchEntityTypeEntry(entityTypeService.findByEntityType(context, "CvPersonClone"))
+                 )))
+                .andExpect(jsonPath("$._links.first.href", Matchers.allOf(
+                        Matchers.containsString("/api/core/entitytypes?"),
+                        Matchers.containsString("page=0"), Matchers.containsString("size=3"))))
+                .andExpect(jsonPath("$._links.prev.href", Matchers.allOf(
+                        Matchers.containsString("/api/core/entitytypes?"),
+                        Matchers.containsString("page=1"), Matchers.containsString("size=3"))))
+                .andExpect(jsonPath("$._links.self.href", Matchers.allOf(
+                        Matchers.containsString("/api/core/entitytypes?"),
+                        Matchers.containsString("page=2"), Matchers.containsString("size=3"))))
+                .andExpect(jsonPath("$._links.next.href", Matchers.allOf(
+                        Matchers.containsString("/api/core/entitytypes?"),
+                        Matchers.containsString("page=3"), Matchers.containsString("size=3"))))
+                .andExpect(jsonPath("$._links.last.href", Matchers.allOf(
+                        Matchers.containsString("/api/core/entitytypes?"),
+                        Matchers.containsString("page=4"), Matchers.containsString("size=3"))))
+                .andExpect(jsonPath("$.page.size", is(3)))
+                .andExpect(jsonPath("$.page.totalElements", is(15)))
+                .andExpect(jsonPath("$.page.totalPages", is(5)))
+                .andExpect(jsonPath("$.page.number", is(2)));
     }
 
+    @Test
+    public void findAllByAuthorizedCollectionByEPersonWithOutCvEntitiesTest() throws Exception {
+        try {
+            context.turnOffAuthorisationSystem();
+
+            parentCommunity = CommunityBuilder.createCommunity(context)
+                                              .withName("Parent Community")
+                                              .build();
+            CollectionBuilder.createCollection(context, parentCommunity)
+                             .withEntityType("JournalIssue")
+                             .withSubmitterGroup(eperson)
+                             .withName("Collection 1")
+                             .build();
+            CollectionBuilder.createCollection(context, parentCommunity)
+                             .withEntityType("Publication")
+                             .withSubmitterGroup(eperson)
+                             .withName("Collection 2")
+                             .build();
+            CollectionBuilder.createCollection(context, parentCommunity)
+                             .withEntityType("CvProject")
+                             .withSubmitterGroup(eperson)
+                             .withName("Collection 3")
+                             .build();
+            CollectionBuilder.createCollection(context, parentCommunity)
+                             .withEntityType("CvPatentClone")
+                             .withSubmitterGroup(eperson)
+                             .withName("Collection 4")
+                             .build();
+            CollectionBuilder.createCollection(context, parentCommunity)
+                             .withEntityType("CvPerson")
+                             .withSubmitterGroup(eperson)
+                             .withName("Collection 5")
+                             .build();
+            CollectionBuilder.createCollection(context, parentCommunity)
+                             .withEntityType("CvPublication")
+                             .withSubmitterGroup(eperson)
+                             .withName("Collection 6")
+                             .build();
+            context.restoreAuthSystemState();
+
+            String token = getAuthToken(eperson.getEmail(), password);
+            getClient(token).perform(get("/api/core/entitytypes/search/findAllByAuthorizedCollection"))
+                            .andExpect(status().isOk())
+                            .andExpect(jsonPath("$._embedded.entitytypes", containsInAnyOrder(
+                    EntityTypeMatcher.matchEntityTypeEntry(entityTypeService.findByEntityType(context, "JournalIssue")),
+                    EntityTypeMatcher.matchEntityTypeEntry(entityTypeService.findByEntityType(context, "Publication"))
+                    )))
+                    .andExpect(jsonPath("$.page.totalPages", is(1)))
+                    .andExpect(jsonPath("$.page.totalElements", is(2)))
+                    .andExpect(jsonPath("$.page.number", is(0)));
+        } finally {
+            CommunityBuilder.deleteCommunity(parentCommunity.getID());
+        }
+    }
+
+    @Test
+    public void findAllByAuthorizedCollectionByAdminWithOutCvEntitiesTest() throws Exception {
+        try {
+            context.turnOffAuthorisationSystem();
+
+            parentCommunity = CommunityBuilder.createCommunity(context)
+                                              .withName("Parent Community")
+                                              .build();
+            CollectionBuilder.createCollection(context, parentCommunity)
+                             .withEntityType("JournalIssue")
+                             .withName("Collection 1")
+                             .build();
+            CollectionBuilder.createCollection(context, parentCommunity)
+                             .withEntityType("Publication")
+                             .withName("Collection 2")
+                             .build();
+            CollectionBuilder.createCollection(context, parentCommunity)
+                             .withEntityType("CvProject")
+                             .withName("Collection 3")
+                             .build();
+            CollectionBuilder.createCollection(context, parentCommunity)
+                             .withEntityType("CvPatentClone")
+                             .withName("Collection 4")
+                             .build();
+            CollectionBuilder.createCollection(context, parentCommunity)
+                             .withEntityType("CvPerson")
+                             .withName("Collection 5")
+                             .build();
+            CollectionBuilder.createCollection(context, parentCommunity)
+                             .withEntityType("CvPublication")
+                             .withName("Collection 6")
+                             .build();
+            context.restoreAuthSystemState();
+
+            String adminToken = getAuthToken(admin.getEmail(), password);
+            getClient(adminToken).perform(get("/api/core/entitytypes/search/findAllByAuthorizedCollection"))
+                                 .andExpect(status().isOk())
+                                 .andExpect(jsonPath("$._embedded.entitytypes", containsInAnyOrder(
+                    EntityTypeMatcher.matchEntityTypeEntry(entityTypeService.findByEntityType(context, "JournalIssue")),
+                    EntityTypeMatcher.matchEntityTypeEntry(entityTypeService.findByEntityType(context, "Publication"))
+                    )))
+                    .andExpect(jsonPath("$.page.totalPages", is(1)))
+                    .andExpect(jsonPath("$.page.totalElements", is(2)))
+                    .andExpect(jsonPath("$.page.number", is(0)));
+        } finally {
+            CommunityBuilder.deleteCommunity(parentCommunity.getID());
+        }
+    }
 }

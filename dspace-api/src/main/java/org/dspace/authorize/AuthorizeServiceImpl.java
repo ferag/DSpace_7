547386ce5ae67afef9e8ad30b322e8332cee69b0
Andrea Bollini <andrea.bollini@4science.it>
2021-09-30 21:10:40 +0200

    Reorder authz check to prioritize the cheaper ones

--- dspace-api/src/main/java/org/dspace/authorize/AuthorizeServiceImpl.java
+++ dspace-api/src/main/java/org/dspace/authorize/AuthorizeServiceImpl.java
@@ -11,9 +11,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Objects;
 import java.util.UUID;
 
 import org.apache.commons.collections4.CollectionUtils;
@@ -63,7 +61,7 @@ import org.springframework.beans.factory.annotation.Autowired;
  */
 public class AuthorizeServiceImpl implements AuthorizeService {
 
-    private static Logger log = LogManager.getLogger(AuthorizeServiceImpl.class);
+    private static final Logger log = LogManager.getLogger();
 
     @Autowired(required = true)
     protected BitstreamService bitstreamService;
@@ -244,25 +242,18 @@ public class AuthorizeServiceImpl implements AuthorizeService {
         // If authorization was given before and cached
         Boolean cachedResult = c.getCachedAuthorizationResult(o, action, e);
         if (cachedResult != null) {
-            return cachedResult.booleanValue();
+            return cachedResult;
         }
 
         // is eperson set? if not, userToCheck = null (anonymous)
         EPerson userToCheck = null;
         if (e != null) {
             userToCheck = e;
+
+            // perform immediately isAdmin check as this is cheap
             if (isAdmin(c, e)) {
                 return true;
             }
-//            // perform isAdmin check to see
-//            // if user is an Admin on this object
-//            DSpaceObject adminObject = useInheritance ? serviceFactory.getDSpaceObjectService(o)
-//                                                                      .getAdminObject(c, o, action) : null;
-//
-//            if (isAdmin(c, e, adminObject)) {
-//                c.cacheAuthorizedAction(o, action, e, true, null);
-//                return true;
-//            }
         }
 
         // In case the dso is an bundle or bitstream we must ignore custom
@@ -284,12 +275,14 @@ public class AuthorizeServiceImpl implements AuthorizeService {
             ignoreCustomPolicies = !isAnyItemInstalled(c, Arrays.asList(((Bundle) o)));
         }
         if (o instanceof Item) {
-            if (workspaceItemService.findByItem(c, (Item) o) != null ||
-                workflowItemService.findByItem(c, (Item) o) != null) {
+            // the isArchived check is fast and would exclude the possibility that the item
+            // is a workspace or workflow without further queries
+            if (!((Item) o).isArchived() &&
+                    (workspaceItemService.findByItem(c, (Item) o) != null ||
+                    workflowItemService.findByItem(c, (Item) o) != null)) {
                 ignoreCustomPolicies = true;
             }
         }
-
 
         for (ResourcePolicy rp : getPoliciesActionFilter(c, o, action)) {
 
@@ -311,7 +304,7 @@ public class AuthorizeServiceImpl implements AuthorizeService {
                 }
 
                 if ((rp.getGroup() != null)
-                    && (groupService.isMember(c, e, rp.getGroup()))) {
+                    && groupService.isMember(c, e, rp.getGroup())) {
                     // group was set, and eperson is a member
                     // of that group
                     c.cacheAuthorizedAction(o, action, e, true, rp);
@@ -326,18 +319,16 @@ public class AuthorizeServiceImpl implements AuthorizeService {
             }
         }
 
-        if (Objects.nonNull(e)) {
-            // perform isAdmin check to see
+        if (e != null) {
             // if user is an Admin on this object
             DSpaceObject adminObject = useInheritance ? serviceFactory.getDSpaceObjectService(o)
-                .getAdminObject(c, o, action) : null;
+                                                                      .getAdminObject(c, o, action) : null;
 
             if (isAdmin(c, e, adminObject)) {
                 c.cacheAuthorizedAction(o, action, e, true, null);
                 return true;
             }
         }
-
         // default authorization is denial
         c.cacheAuthorizedAction(o, action, e, false, null);
         return false;
@@ -381,7 +372,7 @@ public class AuthorizeServiceImpl implements AuthorizeService {
 
         Boolean cachedResult = c.getCachedAuthorizationResult(o, Constants.ADMIN, e);
         if (cachedResult != null) {
-            return cachedResult.booleanValue();
+            return cachedResult;
         }
 
         //
@@ -398,7 +389,7 @@ public class AuthorizeServiceImpl implements AuthorizeService {
                 }
 
                 if ((rp.getGroup() != null)
-                    && (groupService.isMember(c, e, rp.getGroup()))) {
+                    && groupService.isMember(c, e, rp.getGroup())) {
                     // group was set, and eperson is a member
                     // of that group
                     c.cacheAuthorizedAction(o, Constants.ADMIN, e, true, rp);
@@ -523,7 +514,7 @@ public class AuthorizeServiceImpl implements AuthorizeService {
         List<ResourcePolicy> policies = getPolicies(c, src);
 
         //Only inherit non-ADMIN policies (since ADMIN policies are automatically inherited)
-        List<ResourcePolicy> nonAdminPolicies = new ArrayList<ResourcePolicy>();
+        List<ResourcePolicy> nonAdminPolicies = new ArrayList<>();
         for (ResourcePolicy rp : policies) {
             if (rp.getAction() != Constants.ADMIN) {
                 nonAdminPolicies.add(rp);
@@ -546,7 +537,7 @@ public class AuthorizeServiceImpl implements AuthorizeService {
     public void addPolicies(Context c, List<ResourcePolicy> policies, DSpaceObject dest)
         throws SQLException, AuthorizeException {
         // now add them to the destination object
-        List<ResourcePolicy> newPolicies = new LinkedList<>();
+        List<ResourcePolicy> newPolicies = new ArrayList<>(policies.size());
 
         for (ResourcePolicy srp : policies) {
             ResourcePolicy rp = resourcePolicyService.create(c);
@@ -621,7 +612,7 @@ public class AuthorizeServiceImpl implements AuthorizeService {
                                            int actionID) throws java.sql.SQLException {
         List<ResourcePolicy> policies = getPoliciesActionFilter(c, o, actionID);
 
-        List<Group> groups = new ArrayList<Group>();
+        List<Group> groups = new ArrayList<>();
         for (ResourcePolicy resourcePolicy : policies) {
             if (resourcePolicy.getGroup() != null && resourcePolicyService.isDateValid(resourcePolicy)) {
                 groups.add(resourcePolicy.getGroup());
@@ -789,6 +780,7 @@ public class AuthorizeServiceImpl implements AuthorizeService {
      * @param context   context with the current user
      * @return          true if the current user is a community admin in the site
      *                  false when this is not the case, or an exception occurred
+     * @throws java.sql.SQLException passed through.
      */
     @Override
     public boolean isCommunityAdmin(Context context) throws SQLException {
@@ -801,6 +793,7 @@ public class AuthorizeServiceImpl implements AuthorizeService {
      * @param context   context with the current user
      * @return          true if the current user is a collection admin in the site
      *                  false when this is not the case, or an exception occurred
+     * @throws java.sql.SQLException passed through.
      */
     @Override
     public boolean isCollectionAdmin(Context context) throws SQLException {
@@ -813,6 +806,7 @@ public class AuthorizeServiceImpl implements AuthorizeService {
      * @param context   context with the current user
      * @return          true if the current user is a community or collection admin in the site
      *                  false when this is not the case, or an exception occurred
+     * @throws java.sql.SQLException passed through.
      */
     @Override
     public boolean isComColAdmin(Context context) throws SQLException {

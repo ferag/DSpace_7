b9d7e514c60286844b78ac48b16336517acf3e1d
Corrado Lombardi <corrado.lombardi@4science.it>
2021-10-05 17:10:25 +0200

    temporarily changed test execution because of current errors on third party services

--- dspace-server-webapp/src/test/java/org/dspace/app/rest/RenacytUpdateIT.java
+++ dspace-server-webapp/src/test/java/org/dspace/app/rest/RenacytUpdateIT.java
@@ -25,6 +25,7 @@ import org.dspace.builder.ItemBuilder;
 import org.dspace.content.Collection;
 import org.dspace.content.Item;
 import org.hamcrest.Matchers;
+import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -34,6 +35,7 @@ import org.junit.Test;
 public class RenacytUpdateIT extends AbstractControllerIntegrationTest {
 
     @Test
+    @Ignore("endpoints currently unavailable")
     public void updateItemWithInformationFromRenacytByCollectionTest() throws Exception {
         context.turnOffAuthorisationSystem();
 
@@ -118,6 +120,7 @@ public class RenacytUpdateIT extends AbstractControllerIntegrationTest {
     }
 
     @Test
+    @Ignore("endpoints currently unavailable")
     public void updateAllItemsWithInformationFromRenacytTest() throws Exception {
         context.turnOffAuthorisationSystem();
 
@@ -203,6 +206,7 @@ public class RenacytUpdateIT extends AbstractControllerIntegrationTest {
     }
 
     @Test
+    @Ignore("endpoints currently unavailable")
     public void updateItemsWithInformationFromReniecByWrongIDColTest() throws Exception {
         context.turnOffAuthorisationSystem();
 
@@ -286,6 +290,7 @@ public class RenacytUpdateIT extends AbstractControllerIntegrationTest {
     }
 
     @Test
+    @Ignore("endpoints currently unavailable")
     public void updateItemByCollectionTest() throws Exception {
         context.turnOffAuthorisationSystem();
 

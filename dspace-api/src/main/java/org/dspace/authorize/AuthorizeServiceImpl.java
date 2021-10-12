e88442e5eaeb1a7db3c05de5aae84833a73795e3
Luca Giamminonni <luca.giamminonni@4science.it>
2021-10-12 16:57:23 +0200

    [CST-4675] Consider the useInheritance value into the authorizedActions cache

--- dspace-api/src/main/java/org/dspace/authorize/AuthorizeServiceImpl.java
+++ dspace-api/src/main/java/org/dspace/authorize/AuthorizeServiceImpl.java
@@ -240,7 +240,7 @@ public class AuthorizeServiceImpl implements AuthorizeService {
         }
 
         // If authorization was given before and cached
-        Boolean cachedResult = c.getCachedAuthorizationResult(o, action, e);
+        Boolean cachedResult = c.getCachedAuthorizationResult(o, action, e, useInheritance);
         if (cachedResult != null) {
             return cachedResult;
         }
@@ -249,9 +249,13 @@ public class AuthorizeServiceImpl implements AuthorizeService {
         EPerson userToCheck = null;
         if (e != null) {
             userToCheck = e;
+            // perform isAdmin check to see
+            // if user is an Admin on this object
+            DSpaceObject adminObject = useInheritance ? serviceFactory.getDSpaceObjectService(o)
+                                                                      .getAdminObject(c, o, action) : null;
 
-            // perform immediately isAdmin check as this is cheap
-            if (isAdmin(c, e)) {
+            if (isAdmin(c, e, adminObject)) {
+                c.cacheAuthorizedAction(o, action, e, useInheritance, true, null);
                 return true;
             }
         }
@@ -299,7 +303,7 @@ public class AuthorizeServiceImpl implements AuthorizeService {
             // check policies for date validity
             if (resourcePolicyService.isDateValid(rp)) {
                 if (rp.getEPerson() != null && rp.getEPerson().equals(userToCheck)) {
-                    c.cacheAuthorizedAction(o, action, e, true, rp);
+                    c.cacheAuthorizedAction(o, action, e, useInheritance, true, rp);
                     return true; // match
                 }
 
@@ -307,7 +311,7 @@ public class AuthorizeServiceImpl implements AuthorizeService {
                     && groupService.isMember(c, e, rp.getGroup())) {
                     // group was set, and eperson is a member
                     // of that group
-                    c.cacheAuthorizedAction(o, action, e, true, rp);
+                    c.cacheAuthorizedAction(o, action, e, useInheritance, true, rp);
                     return true;
                 }
             }
@@ -330,7 +334,7 @@ public class AuthorizeServiceImpl implements AuthorizeService {
             }
         }
         // default authorization is denial
-        c.cacheAuthorizedAction(o, action, e, false, null);
+        c.cacheAuthorizedAction(o, action, e, useInheritance, false, null);
         return false;
     }
 

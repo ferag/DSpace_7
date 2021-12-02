e3f0bbf3327c6ba21d815cc6c44a2d77c0afd8ab
Mykhaylo <michele.boychuk@gmail.com>
2021-12-02 09:29:16 +0100

    [CSTPER-934] fix license and checkstyle

--- dspace-api/src/main/java/org/dspace/app/elasticsearch/externalservice/ElasticsearchProvider.java
+++ dspace-api/src/main/java/org/dspace/app/elasticsearch/externalservice/ElasticsearchProvider.java
@@ -7,14 +7,11 @@
  */
 package org.dspace.app.elasticsearch.externalservice;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
 import java.sql.SQLException;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
 
-import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
